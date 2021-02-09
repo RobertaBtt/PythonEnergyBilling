6e9f6ab9ebbf16b623d4c1dc9c166ad140bce9bb
RobertaBtt <roberta.btt@gmail.com>
2021-02-09 10:56:13 +0100

    Added test to deserialization of the json of the readings. Tested the All and one account

--- load_meterings.py
+++ load_meterings.py
@@ -20,14 +20,31 @@ def YAML(readings, key):
     raise NotImplementedError
 
 
-def deserialize_one(meterings:List, account_id:str, service_type: str):
-    if meterings is not None and isinstance(meterings, List):
-        for metering in meterings:
+def deserialize_list(readings:List, member_id:str, account_id:str, service_type):
+    account_list = []
+    try:
+        member_id = get(readings, "JSON", member_id)
+        if account_id == "ALL":
+            # This is a list of dictionaries with key <<account_id>>
+            for accounts in member_id:
+                #A member can have more than one account
+                for account_id, values in accounts.items():
+                    account_list.append(deserialize_one(member_id, account_id, service_type))
+        else:
+            return deserialize_one(member_id, account_id, service_type)
+        return account_list
+
+    except Exception as ex:
+        raise ex
+
+def deserialize_one(readings:List, account_id:str, service_type: str):
+    if readings is not None and isinstance(readings, List):
+        for metering in readings:
             if account_id in metering:
                 try:
                     meterings_for_account = get(metering, "JSON", account_id)
-                    for meterings in meterings_for_account:
-                        meter_reading_list = get(meterings, "JSON", service_type)
+                    for readings in meterings_for_account:
+                        meter_reading_list = get(readings, "JSON", service_type)
                     return meter_reading_list
 
                 except Exception as ex:
--- test_load_meterings.py
+++ test_load_meterings.py
@@ -1,33 +1,50 @@
 import unittest
 from load_readings import get_readings
 from load_meterings import deserialize_one
+from load_meterings import deserialize_list
 
 class TestLoadMeterings(unittest.TestCase):
 
     def setUp(self) -> None:
         self.readings = get_readings()
-        self.account_id="account-abc"
-        self.service_type="electricity"
+        self.account_id = "account-abc"
+        self.service_type = "electricity"
+        self.account_fake = "account_fake"
+        self.member_id = "member-123"
+        self.service_fake = "smarthome"
+        self.all_accounts = "ALL"
 
     def testDeserializeOne(self):
-        content = self.readings["member-123"]
+        content = self.readings[self.member_id]
         result = deserialize_one(content, self.account_id, self.service_type)
         assert result is not None
         assert len(result) == 14
 
     def testDeserializeOneAccountUnavailable(self):
-        content = self.readings["member-123"]
+        content = self.readings[self.member_id]
         with self.assertRaises(KeyError):
-            deserialize_one(content, "account_fake", self.service_type)
+            deserialize_one(content, self.account_fake, self.service_type)
 
     def testDeserializeOneServiceTypeNotValid(self):
-        content = self.readings["member-123"]
+        content = self.readings[self.member_id]
         with self.assertRaises(KeyError):
-            deserialize_one(content, "account-abc", "smarthome")
+            deserialize_one(content, self.account_id, self.service_fake)
 
     def testDeserializeOneEmpty(self):
-        assert deserialize_one(None, "account-abc", "smarthome") == None
+        assert deserialize_one(None, self.account_id, self.service_fake) is None
 
     def testDeserializeOneNotAList(self):
-        assert deserialize_one(1, "account-abc", "smarthome") == None
+        assert deserialize_one(1, self.account_id, self.service_fake) is None
 
+    def testDeserializeList(self):
+        result = deserialize_list(self.readings, self.member_id, self.account_id,  self.service_type)
+        assert result is not None
+        assert(len(result) == 14)
+
+    def testDeserializeListAllAccounts(self):
+        #When accounts are "ALL", the result is a list of the list of the accounts.
+        result = deserialize_list(self.readings, self.member_id, self.all_accounts,  self.service_type)
+        assert result is not None
+        #This is a list of lists
+        assert(len(result) == 1)
+        assert (len(result[0]) == 14)
